7daab6590607fea416136f09e38e6803934fbe20
Mariya <mariya.emv@gmail.com>
2018-07-18 10:39:06 -0400

    work on proxy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import LoginForm from './LoginForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <LoginForm />
       </div>
     );
   }
---
+++ src/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <form>
+                <input/>
+            </form>
+        )
+    }
+}
+
+export default LoginForm;
