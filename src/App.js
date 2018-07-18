276d712b6fda4ba9066791bffa71de5a45d1ed39
Mariya <mariya.emv@gmail.com>
2018-07-18 16:11:01 -0400

    add componentWillMount

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import LoginForm from './LoginForm';
 
 class App extends Component {
+
+  state = {
+    loading: true,
+  }
+
+  componentWillMount = async () => {
+    const response = await axios.get('/api/petApp')
+    const json = await response.data;
+    console.log(json.randomURL)
+    if(json) this.setState({ loading:false })
+  }
+
   render() {
     return (
       <div className="App">
-        <LoginForm />
+        {
+          !this.state.loading && 
+          <LoginForm />
+        }
       </div>
     );
   }
